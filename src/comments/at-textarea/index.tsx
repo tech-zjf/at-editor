10fa200c19dbb1d1d0797fcf4b9de90e3ffc3699
“tech-zjf” <907572438@qq.com>
2022-06-28 18:03:06 +0800

    update: 获取光标索引&@索引

--- src/comments/at-textarea/index.tsx
+++ src/comments/at-textarea/index.tsx
@@ -1,16 +1,48 @@
 import styles from './index.less'
+
+let content: string = ""
+
+let cursorIndex!: number
+
+let atIndex!: number
+
 const AtTextarea = () => {
+
+
+    const editorClick = () => {
+        // 1.点击、change 获取光标的索引
+        cursorIndex = getCursorIndex()
+
+        // 获取光标左边的
+        atIndex = content.slice(0, cursorIndex + 1).lastIndexOf('@')
+
+        if (atIndex !== -1) {
+            console.log(atIndex, cursorIndex);
+        }
+
+    }
+
+    // 编辑器change
     const editorChange = (e: any) => {
-        const str = e.target.value
-        console.log(str)
+        content = e.target.value
+        editorClick()
     }
-    const editorClick = (event: any) => {
-        console.log('click', event);
+
+    // 获取光标索引
+    const getCursorIndex = () => {
+        return (document.getElementById('textareaRef') as any).selectionStart - 1
     }
+
     return (
         <div>
             {/* 编辑器 */}
-            <textarea className={styles.editorDiv} rows={5} onInput={editorChange} onClick={editorClick}></textarea>
+            <textarea
+                id="textareaRef"
+                className={styles.editorDiv}
+                rows={5}
+                onInput={editorChange}
+                onClick={editorClick} >
+            </textarea>
         </div>
     )
 }
